3642ca3d3bad00bb99a6e0bdb90935e0750d1dd5
berkin tatlısu <berkin@msn.com>
2020-06-24 13:59:11 +0300

    Initial Commit

---
+++ dsjadk/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dsjadk
+//
+//  Created by Berkin Tatlısu on 24.06.2020.
+//  Copyright © 2020 Berkin Tatlısu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
